45121d98a0af76a5eb0e730294607c88ea652e53
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-11-15 15:00:33 -0300

    Alarme

---
+++ src/Metodos/Alarme.java
@@ -0,0 +1,10 @@
+package Metodos;
+
+public class Alarme {
+    public static void main(String[] args) {
+        AlarmeConfig alarme = new AlarmeConfig();
+
+        System.out.println(alarme.agendarAlarme(12, 30));
+    }
+
+}
---
+++ src/Metodos/AlarmeConfig.java
@@ -0,0 +1,8 @@
+package Metodos;
+
+public class AlarmeConfig {
+
+    static String agendarAlarme(int hora, int minutos){
+        return "Seu alarme foi configurado ás " + hora + ":" + minutos;
+    }
+}
